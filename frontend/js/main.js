4971c7bf8884bf9e816e3a8f7484673c04f0e1aa
FoekeBoersma <foekeboersma@hotmail.com>
2026-03-07 17:36:40 +0100

    feat: download route as geojson

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,30 @@
-import { fetchPlace, fetchRoute } from './api.js';
+import { fetchPlace, fetchRoute, lastRouteGeoJSON, lastOrigin, lastDestination } from './api.js';
 import { map } from "./map.js";
 
 const searchBtn = document.getElementById('search');
 const routeBtn = document.getElementById('route-btn');
+const downloadBtn = document.getElementById('download-btn');
+const statusEl = document.getElementById('status');
 
 searchBtn.addEventListener('click', () => fetchPlace());
 routeBtn.addEventListener('click', fetchRoute)
+downloadBtn.addEventListener('click', () => {
+    statusEl.textContent = 'downloading...';
+    if(!lastRouteGeoJSON) {
+        alert("No route to download. Please fetch a route first.");
+        return;
+    }
+
+    const blob = new Blob([JSON.stringify(lastRouteGeoJSON, null, 2)], {
+        type: "application/geo+json"
+    });
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `route${encodeURIComponent(lastOrigin)}_${encodeURIComponent(lastDestination)}.geojson`
+    a.click()
+
+    URL.revokeObjectURL(url)
+});
