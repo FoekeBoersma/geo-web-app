c3aeea20168bf0d4e057246cfa777fa5f662ca90
FoekeBoersma <foekeboersma@hotmail.com>
2026-04-04 12:41:23 +0200

    chore: define two tables in database configuration

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,18 @@
 from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = "sqlite:///./route.db"
+ROUTE_DATABASE_URL = "sqlite:///./route.db" 
+POINTS_DATABASE_URL = "sqlite:///.points.db"
 
-engine = create_engine(
-    DATABASE_URL,
+route_engine = create_engine(
+    ROUTE_DATABASE_URL,
     echo = True  # shows SQL in console
 )
 
+points_engine = create_engine(
+    POINTS_DATABASE_URL,
+    echo = True
+)
+
 def init_db():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(route_engine) # create tables for routing logs
+    SQLModel.metadata.create_all(points_engine) # create tables for points of interest
